56907890e183ceca7b5a4f23c8fc64f2700f0aae
Jossie1708 <104351006+Jossie1708@users.noreply.github.com>
2024-08-31 16:49:57 +0200

    icons added

--- app/src/main/java/com/frogstore/droneapp/HomeFragment.kt
+++ app/src/main/java/com/frogstore/droneapp/HomeFragment.kt
@@ -1,22 +1,22 @@
-package com.frogstore.droneapp.fragments
+package com.frogstore.droneapp
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.frogstore.droneapp.R
 
-class SettingsFragment : Fragment() {
-
+class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val layout = inflater.inflate(R.layout.fragment_settings, container, false)
+        val layout = inflater.inflate(R.layout.fragment_home, container, false)
         // Return the inflated layout
         return layout
 
+
     }
+
 }
