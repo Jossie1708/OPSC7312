bf0fe524076a551b253305ad5f1fc9094c6b2973
Jossie1708 <104351006+Jossie1708@users.noreply.github.com>
2024-09-27 16:27:52 +0200

    theme adjustments

--- app/src/main/java/com/frogstore/droneapp/Fragments/SettingsFragment.kt
+++ app/src/main/java/com/frogstore/droneapp/Fragments/SettingsFragment.kt
@@ -1,15 +1,21 @@
 package com.frogstore.droneapp.Fragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Switch
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.frogstore.droneapp.R
 
 class SettingsFragment : Fragment() {
+
+    private lateinit var switchDarkTheme: Switch
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,22 +28,38 @@ class SettingsFragment : Fragment() {
         val btnPass = layout.findViewById<Button>(R.id.btnSetPassword)
         val btnFingerprint = layout.findViewById<Button>(R.id.btnSetFingerprint)
 
+        // Initialize SharedPreferences
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        switchDarkTheme = layout.findViewById(R.id.switch1)
+
+        // Set the switch state based on saved preference
+        val isDarkTheme = sharedPreferences.getBoolean("isDarkTheme", false)
+        switchDarkTheme.isChecked = isDarkTheme
+
+        // Switch listener to change theme
+        switchDarkTheme.setOnCheckedChangeListener { _, isChecked ->
+            sharedPreferences.edit().putBoolean("isDarkTheme", isChecked).apply()
+            // Restart the activity to apply the new theme
+            requireActivity().recreate()
+        }
+
+
+
         btnDetails.setOnClickListener {
-            // Navigate to AccountDetailsFragment
             findNavController().navigate(R.id.action_nav_settings_to_accountDetailsFragment)
         }
 
         btnPass.setOnClickListener {
-            // Navigate to UpdatePasswordFragment
             findNavController().navigate(R.id.action_nav_settings_to_updatePasswordFragment)
         }
 
         btnFingerprint.setOnClickListener {
-            // Navigate to SetFingerprintFragment
             findNavController().navigate(R.id.action_nav_settings_to_setFingerprintFragment)
         }
 
         // Return the inflated layout
         return layout
     }
+
+
 }
