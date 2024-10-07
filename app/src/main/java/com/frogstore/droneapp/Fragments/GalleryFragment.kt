ba386da6e2a5bf3ee7bde9424c88a8b8c83749d1
Duncan Hattingh <duncanhatting@gmail.com>
2024-10-07 08:11:36 +0200

    updated

--- app/src/main/java/com/frogstore/droneapp/Fragments/GalleryFragment.kt
+++ app/src/main/java/com/frogstore/droneapp/Fragments/GalleryFragment.kt
@@ -1,9 +1,9 @@
 package com.frogstore.droneapp
 
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.os.Environment
 import android.os.storage.StorageManager
 import android.view.LayoutInflater
 import android.view.View
@@ -39,14 +39,22 @@ class GalleryFragment : Fragment() {
 
         return layout
     }
+
+    @SuppressLint("NewApi")
     private fun loadImagesFromFolder() {
-        val folderPath = Environment.getExternalStorageDirectory().path + "/DCIM/100PINT/Pins" // Specify your folder path here
+        val storageManager = requireContext().getSystemService(StorageManager::class.java)
+        val storageVolume = storageManager?.storageVolumes?.get(0)
+        val folderPath =
+            storageVolume?.directory?.path + "/DCIM/100PINT/Pins" // Specify your folder path here
         val folder = File(folderPath)
 
         if (folder.exists() && folder.isDirectory) {
             // Load all image files from the folder
             val imageFiles = folder.listFiles { file ->
-                file?.isFile == true && (file.name.endsWith(".jpeg", true) || file.name.endsWith(".jpg", true) || file.name.endsWith(".png", true))
+                file?.isFile == true && (file.name.endsWith(
+                    ".jpeg",
+                    true
+                ) || file.name.endsWith(".jpg", true) || file.name.endsWith(".png", true))
             }?.map { it.absolutePath } ?: emptyList()
 
             if (imageFiles.isNotEmpty()) {
