59ad0e48aeb454cf1179f7f745eaafa4f3a18e5f
Duncan <duncanhatting@gmail.com>
2024-11-01 09:37:04 +0200

    Neatened up the backend of login

--- app/src/main/java/com/frogstore/droneapp/UserDetails/AccountManager.kt
+++ app/src/main/java/com/frogstore/droneapp/UserDetails/AccountManager.kt
@@ -8,20 +8,29 @@ import androidx.credentials.exceptions.CreateCredentialException
 import android.os.Bundle
 
 class AccountManager(private val activity: Activity) {
+
     private val credentialManager = CredentialManager.create(activity)
 
-    suspend fun signUp(username: String, password: String, email: String): SignUpResult {
-        return try {
+    suspend fun signUp(username: String, password: String, email: String): SignUpResult
+    {
+        return try
+        {
             val request = CreatePasswordRequest(username, password)
-            credentialManager.createCredential(
-                context = activity,
-                request = request
-            )
+
+            credentialManager.createCredential(context = activity, request = request)
+
             SignUpResult.Success(username, email)
-        } catch (e: CreateCredentialCancellationException) {
+
+        }
+
+        catch (e: CreateCredentialCancellationException)
+        {
             e.printStackTrace()
             SignUpResult.Cancelled
-        } catch (e: CreateCredentialException) {
+        }
+
+        catch (e: CreateCredentialException)
+        {
             e.printStackTrace()
             SignUpResult.Failure
         }
